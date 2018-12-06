a0bea5a1bf690e0fce79d21a5cb7bd9bfd2daa2e
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-12-07 00:40:13 +0300

    Added a local Emacs config to setup path translation for SLY and Docker.
    Emacs plugin docker-tramp is required to make this work.

---
+++ .local.el
@@ -0,0 +1,18 @@
+
+
+(add-to-list 'sly-filename-translations
+             (sly-create-filename-translator
+              :machine-instance "ultralisp_app_run_2"
+              :remote-host "ultralisp_app_run_2"
+              :username "root"))
+
+
+(defun sly-make-tramp-file-name (username remote-host lisp-filename)
+  "Old (with multi-hops) tramp compatability function"
+  (message "Makining tramp filename for %s" lisp-filename)
+  (tramp-make-tramp-file-name "docker"
+                              username
+                              nil
+                              remote-host
+                              nil
+                              lisp-filename))
