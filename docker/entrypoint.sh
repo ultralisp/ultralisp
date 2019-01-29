24a861e979e8bf3e89ea6108631935a995db90e0
Alexander Artemenko <svetlyak.40wt@gmail.com>
2019-01-29 09:23:18 +0300

    Worker now started in one shot mode when it processes a task and dies.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,7 @@ if [[ "$MODE" == "worker" ]]; then
     ~/.roswell/bin/qlot exec /app/roswell/worker.ros \
                         --interface "0.0.0.0" \
                         --port 10100 \
+                        --one-task-only \
                         --slynk-interface "0.0.0.0" \
                         --slynk-port 4005 \
         | tee -a /app/logs/worker.log \
