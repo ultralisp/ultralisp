e9c2da327f2a7e18b9ff97ed7e96d6c74093a57b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2019-03-10 11:47:50 +0300

    Switched back to running only one worker process.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,14 @@ set -e
 set -x
 
 if [[ "$MODE" == "worker" ]]; then
-    /usr/bin/supervisord --nodaemon
+    /app/worker \
+        --interface "0.0.0.0" \
+        --port "10100" \
+        --one-task-only \
+        --slynk-interface "0.0.0.0" \
+        --slynk-port "4005" \
+        | tee -a /app/logs/worker.log \
+        | jsail
 else
     /app/ultralisp-server \
         | tee -a /app/logs/app.log \
