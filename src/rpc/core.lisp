3a0dedcb4bfad52cf316ae328354d605a4b14398
Alexander Artemenko <svetlyak.40wt@gmail.com>
2019-05-05 20:06:15 +0300

    Moved from lfarm to gearman for remote task execution.

---
+++ src/rpc/core.lisp
@@ -0,0 +1,61 @@
+(defpackage #:ultralisp/rpc/core
+  (:use #:cl)
+  (:use #:log4cl)
+  (:import-from #:log4cl-json)
+  (:import-from #:slynk)
+  (:import-from #:cl-store)
+  (:import-from #:cl-gearman)
+  
+  (:import-from #:ultralisp/slynk)
+  (:import-from #:ultralisp/rpc/command
+                #:task-with-commands
+                #:with-commands-processor)
+  (:import-from #:ultralisp/variables
+                #:get-gearman-server
+                #:get-postgres-ro-pass
+                #:get-postgres-ro-user
+                #:get-postgres-host)
+  (:export
+   #:submit-task))
+(in-package ultralisp/rpc/core)
+
+
+(defun serialize (object)
+  (base64:usb8-array-to-base64-string
+   (flex:with-output-to-sequence (stream)
+     (cl-store:store object stream))))
+
+(defun deserialize (base64-string)
+  (flex:with-input-from-sequence
+      (stream (base64:base64-string-to-usb8-array base64-string))
+    (cl-store:restore stream)))
+
+
+(defun gearman-call (function-name &rest args)
+  (let ((returned-value nil))
+    ;; May be we need to process a CL-GEARMAN::GEARMAN-CONNECTION-ERROR
+    ;; here. This condition will be thrown in case, if gearman server is
+    ;; unavailable.
+    (cl-gearman:with-client (client (get-gearman-server))
+      (log:info "Submitting job")
+      (let* ((raw-result (cl-gearman:submit-job client
+                                                function-name
+                                                :arg (serialize args)))
+             (result (deserialize raw-result)))
+        (log:info "Result received" result)
+        (setf returned-value result)))
+    returned-value))
+
+
+(defun submit-task (func &rest args)
+  "Submits a task to one of remote workers and waits for the result."
+  (check-type func symbol)
+  
+  (with-commands-processor 
+    (apply #'gearman-call
+           "task-with-commands"
+           (get-postgres-host)
+           (get-postgres-ro-user)
+           (get-postgres-ro-pass)
+           func args)))
+
--- src/worker.lisp
+++ src/worker.lisp
@@ -2,9 +2,8 @@
   (:use #:cl)
   (:import-from #:defmain
                 #:defmain)
-
-  (:import-from #:ultralisp/lfarm/core
-                #:*after-last-task*)
+  (:import-from #:ultralisp/variables
+                #:get-gearman-server)
 
   ;; These dependencies need for a worker to process version builds
   (:import-from #:ultralisp/downloader/github)
@@ -16,11 +15,38 @@
 (in-package ultralisp/worker)
 
 
-(defmain main ((port "A port to listen on."
-                     :default 10100)
-               (interface "A interface to listen on."
-                          :default "localhost")
-               (slynk-port "A port to listen for connection from SLY."
+(defun serialize (object)
+  (base64:usb8-array-to-base64-string
+   (flex:with-output-to-sequence (stream)
+     (cl-store:store object stream))))
+
+(defun deserialize (base64-string)
+  (flex:with-input-from-sequence
+      (stream (base64:base64-string-to-usb8-array base64-string))
+    (cl-store:restore stream)))
+
+
+(defun process-jobs (&key one-task-only)
+  ;; May be we need to process a CL-GEARMAN::GEARMAN-CONNECTION-ERROR
+  ;; here. This condition will be thrown in case, if gearman server is
+  ;; unavailable.
+  (cl-gearman:with-worker (worker (get-gearman-server)) 
+    (cl-gearman:add-ability worker "task-with-commands"
+                            (lambda (arg job)
+                              (declare (ignorable job))
+                              (let ((args (deserialize arg)))
+                                (log:info "Processing task" args)
+                                (serialize
+                                 (apply #'ultralisp/rpc/command:task-with-commands
+                                        args)))))
+    
+    (cond
+      (one-task-only (cl-gearman:work worker)
+                     (log:info "One job was processed, exiting."))
+      (t (loop (cl-gearman:work worker))))))
+
+
+(defmain main ((slynk-port "A port to listen for connection from SLY."
                            :default 10200
                            :short nil)
                (slynk-interface "An interface to listen on for connection from SLY."
@@ -34,15 +60,9 @@
   ;; To add a new line before any JSON log items will go
   (format t "~2&")
   
-  (cond (debug
-         (log4cl-json:setup :level :info)
-         (setf lfarm-common:*log-level* :info))
-        (t
-         (log4cl-json:setup :level :error)
-         (setf lfarm-common:*log-level* :error)))
-
-  (when one-task-only
-    (setf *after-last-task* 'ultralisp/lfarm/core::on-last-task))
+  (log4cl-json:setup :level (if debug
+                                :info
+                                :error))
 
   ;; To make it possible to connect to a remote SLYNK server where ports are closed
   ;; with firewall.
@@ -53,7 +73,5 @@
                        :port slynk-port
                        :interface slynk-interface)
   
-  (log:info "Starting lfarm server")
-  
-  (lfarm-server:start-server interface
-                             port))
+  (log:info "Waiting for tasks")
+  (process-jobs :one-task-only one-task-only))
