fc0fb8f0210dfa46370b552cc7565e62b6870dce
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-01-12 10:45:46 +0300

    Added a link to all user project from the page title.

--- src/widgets/project.lisp
+++ src/widgets/project.lisp
@@ -38,6 +38,8 @@
                 #:make-source-widget)
   (:import-from #:fare-utils
                 #:push-last-item!)
+  (:import-from #:rutils
+                #:fmt)
   (:export
    #:make-project-widget))
 (in-package ultralisp/widgets/project)
@@ -112,87 +114,34 @@
 
 
 (defun render-project (widget)
-  (let* (;; (actions (get-project-actions project))
-         ;; TODO: Optimize get-version
-         ;; seconds  |     gc     |   consed  | calls |  sec/call  |  name
-         ;; -------------------------------------------------------
-         ;;      0.190 |      0.000 | 3,454,016 |     1 |   0.189999 | GET-VERSIONS
-         ;;      0.080 |      0.000 |   776,720 |     1 |   0.079999 | GET-PROJECT-ACTIONS
-         ;;      0.040 |      0.000 |   410,864 |     3 |   0.013327 | RENDER
-         ;;      0.020 |      0.000 |   275,248 |     1 |   0.019994 | RENDER-PROJECT
-         ;;      0.000 |      0.000 |         0 |     1 |   0.000000 | GET-DESCRIPTION
-         ;;      0.000 |      0.000 |         0 |     1 |   0.000000 | GET-EXTERNAL-URL
-         ;;      0.000 |      0.000 |   163,824 |     1 |   0.000000 | GET-PROJECT-CHECKS
-         ;; -------------------------------------------------------
-         ;;      0.330 |      0.000 | 5,080,672 |     9 |            | Total
-         ;; (versions (get-versions project))
-         (project (project widget))
+  (let* ((project (project widget))
          (project-name (project-name widget))
-         (description (ultralisp/models/project:project-description project))
-         ;; (next-check-at (get-time-of-the-next-check project))
-         ;; (checks (get-project-checks project :pending t))
-         ;; (current-user-is-moderator
-         ;;   (is-moderator project
-         ;;                 (get-current-user)))
-         ;; (not-moderator
-         ;;   (not current-user-is-moderator))
-         ;; (changelog (sort nil
-         ;;                  ;; (append actions
-         ;;                  ;;         versions
-         ;;                  ;;         checks)
-         ;;                  #'local-time:timestamp>
-         ;;                  ;; We want last actions came first
-         ;;                  :key #'mito:object-updated-at))
-         ;; (is-enabled (is-enabled-p project))
-         ;; (reason (unless is-enabled
-         ;;           (ultralisp/models/project:get-disable-reason project)))
-         ;; (reason-description
-         ;;   (when reason
-         ;;     (case reason
-         ;;       (:manual "Turned off manually")
-         ;;       (:system-conflict "System conflict")
-         ;;       (t (format nil "~A" reason)))))
-         )
+         (description (ultralisp/models/project:project-description project)))
     (setf (get-title)
-          (format nil "~A – ~A"
-                  project-name
-                  description))
-
-
+          (fmt "~A – ~A"
+               project-name
+               description))
+    
     (with-html
       ;; Show a list of versions where it was included
       (:h1 :class "full-name"
-           ;; (:a :class "author-name"
-           ;;     :href (format nil "/projects/~A" author-name)
-           ;;     author-name)
-           ;; "/"
-           (:span :class "project-name"
-                  project-name)
-           ;; TODO: надо сделать отображение источников project2
-           ;; (render-switch is-enabled
-           ;;                (lambda (&rest args)
-           ;;                  (declare (ignorable args))
-           ;;                  (toggle widget project))
-           ;;                :disabled not-moderator
-           ;;                :title (when not-moderator
-           ;;                         "You are not a moderator of this project"))
-           ;; (when reason-description
-           ;;   (:span :class "disable-reason"
-           ;;          reason-description))
-           )
+           (destructuring-bind (user-name project-name)
+               (str:split #\/ project-name :limit 2)
+             (:a :class "user-name"
+                 :href (fmt "/projects/~A"
+                            user-name)
+                 user-name)
+             (:span :class "separator"
+                    "/")
+             (:span :class "project-name"
+                    project-name)))
       (:h2 :class "project-description"
            description)
 
       (loop for item in (source-widgets widget)
             do (render item))
 
-      (render (add-form widget))
-      
-      ;; (ultralisp/widgets/changelog:render (cons (make-instance 'next-check
-      ;;                                                          :at next-check-at)
-      ;;                                           changelog)
-      ;;                                     :timestamps t)
-      )))
+      (render (add-form widget)))))
 
 
 (defmethod render ((widget project))
