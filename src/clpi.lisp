4f09c70da4c7932ab7097143a80d98313c0ac087
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-09-30 01:46:22 +0300

    Fixed duplication of some data. Now CLPI is generated for the given DIST object.

--- src/clpi.lisp
+++ src/clpi.lisp
@@ -9,8 +9,7 @@
 (in-package ultralisp/clpi)
 
 
-(defun project-versions (project)
-  ;; TODO: filter dist for which CLPI is built
+(defun project-versions (dist project)
   (mapcar #'second
           (mito:retrieve-by-sql 
            "select dist.quicklisp_version
@@ -21,21 +20,23 @@
               join dist on dist.id = dist_source.dist_id
                and dist.version = dist_source.dist_version
              where project2.id = ?
+               and dist.id = ?
                and dist.state = 'ready'
                and dist_source.enabled = true
           order by dist.created_at"
-           :binds (list (mito:object-id project)))))
+           :binds (list (mito:object-id project)
+                        (mito:object-id dist)))))
 
 
-(defun write-project (project stream)
+(defun write-project (dist project stream)
   (write (list* (ultralisp/models/project:project-name project)
-                (project-versions project))
+                (project-versions dist project))
          :stream stream)
   (terpri stream)
   (values))
 
 
-(defun get-project-systems-info (project)
+(defun get-project-systems-info (dist project)
   (loop with rows = (mito:retrieve-by-sql
                      "with rows as (
                            select project2.name as project,
@@ -51,6 +52,7 @@
                                on dist.id = dist_source.dist_id
                               and dist.version = dist_source.dist_version
                             where project2.id = ?
+                              and dist.id = ?
                               and dist.state = 'ready'
                               and dist_source.enabled = true
                             order by dist.created_at
@@ -59,7 +61,8 @@
                             jsonb_agg(jsonb_build_array(jsonb_build_array(project, version))) as project_versions
                        from rows
                       group by system"
-                     :binds (list (mito:object-id project)))
+                     :binds (list (mito:object-id project)
+                                  (mito:object-id dist)))
         for row in rows
         for system = (getf row :system)
         for project-data = (getf row :project-versions)
@@ -68,8 +71,8 @@
                        project-versions)))
 
 
-(defun write-project-systems (project stream)
-  (loop for system in (get-project-systems-info project)
+(defun write-project-systems (dist project stream)
+  (loop for system in (get-project-systems-info dist project)
         do (write system :stream stream)
            (terpri stream))
   (values))
@@ -94,7 +97,10 @@
                               collect (cons filename systems)))))
 
 
-(defun get-project-releases (project)
+(defun get-project-releases (dist project)
+  ;; TODO: In future we need to extract description of each ASDF system
+  ;; and store them in the database. But for now we'll put projects'
+  ;; description to the index.
   (loop with rows = (mito:retrieve-by-sql
                      "with rows as (
        select dist.quicklisp_version as version,
@@ -111,6 +117,7 @@
            on dist.id = dist_source.dist_id
           and dist.version = dist_source.dist_version
         where project2.id = ?
+          and dist.id = ?
           and dist.state = 'ready'
           and dist_source.enabled = true
         order by dist.created_at),
@@ -136,26 +143,36 @@
                                            'filename', filename)) as systems
         from preprocessed
        group by version, url, size, md5"
-                     :binds (list (mito:object-id project)))
+                     :binds (list (mito:object-id project)
+                                  (mito:object-id dist)))
         for row in rows
         collect (list (getf row :version)
                       :url (getf row :url)
                       :size (getf row :size)
                       :md5 (getf row :md5)
+                      ;; TODO: also we need to extract license information
+                      ;; :license ????
+                      ;; TODO: extract and store system version if given in ASD
+                      ;; :version ??? ;; Dotted string
                       :systems (group-systems-by-file (getf row :systems)))))
 
 
-(defun write-project-releases (project stream)
-  (loop for release in (get-project-releases project)
+(defun write-project-releases (dist project stream)
+  (loop for release in (get-project-releases dist project)
         do (write release :stream stream)
            (terpri stream)))
 
 
-(defun write-systems-info (systems-dir project)
-  ;; TODO: filter dist for which CLPI is built and latest source
+(defun write-systems-info (dist systems-dir project)
   (loop for source in (ultralisp/models/project:project-sources project)
-        for systems = (ultralisp/models/source:source-systems-info
-                       source)
+        for systems = (let* ((source-dists (ultralisp/models/dist-source:source-distributions source))
+                             (source-dist-ids (mapcar #'ultralisp/models/dist-source:dist-id
+                                                      source-dists)))
+                        ;; Were are only interested in the sources bound to the current dist
+                        (when (member (mito:object-id dist)
+                                      source-dist-ids)
+                          (ultralisp/models/source:source-systems-info
+                           source)))
         for project-name = (ultralisp/models/project:project-name project)
         do (loop for system in systems
                  for system-name = (quickdist:get-name system)
@@ -172,7 +189,7 @@
   (values))
 
 
-(defun write-index (projects &key (base-dir #P"clpi/clpi/v0.4/"))
+(defun write-index (dist projects &key (base-dir #P"clpi/clpi/v0.4/"))
   (ultralisp/db:with-connection ()
     (let* ((clpi-version-file (merge-pathnames #P"clpi-version" base-dir))
            (projects-index-file (merge-pathnames #P"project-index" base-dir))
@@ -196,22 +213,23 @@
                                                      projects-dir)
                 for version-scheme-path = (merge-pathnames (format nil "~A/version-scheme" project-name)
                                                            projects-dir)
-                do (write-project project projects-stream)
-                   (write-project-systems project systems-stream)
+                do (write-project dist project projects-stream)
+                   (write-project-systems dist project systems-stream)
                   
                    (ensure-directories-exist releases-path)
                    (with-open-file (releases-stream releases-path :direction :output
                                                                   :if-exists :supersede)
-                     (write-project-releases project releases-stream))
+                     (write-project-releases dist project releases-stream))
                    ;; TODO: probably, when we'll make version extraction out of ASDF,
                    ;;       we'll need to guess version type and write :DATE or :SEMANTIC
                    ;;       depending on actual version numbering of the project.
                    (with-open-file (stream version-scheme-path :direction :output
                                                                :if-exists :supersede)
                      (write :date :stream stream))
-                   (write-systems-info systems-dir project))))))
+                   (write-systems-info dist systems-dir project))))))
   (values))
 
 
 (defun write-index-for-dist (dist)
-  (write-index (ultralisp/models/project:get-all-dist-projects dist)))
+  (write-index dist
+               (ultralisp/models/project:get-all-dist-projects dist)))
