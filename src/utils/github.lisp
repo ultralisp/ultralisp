cc5c677bb7329bacf88f522aec1ee775b9b465be
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-09-18 20:33:15 +0000

    Use cl-github to retrieve repository tags.
    This fixes issue #157.

--- src/utils/github.lisp
+++ src/utils/github.lisp
@@ -10,13 +10,8 @@
                 #:with-timeout)
   (:export
    #:get-branches
-   #:extract-github-name
-   #:*token*))
+   #:extract-github-name))
 (in-package ultralisp/utils/github)
-
-
-(defvar *token* nil
-  "If given, will be used to make authenticated requests.")
 
 
 (defun extract-github-name (url)
@@ -33,27 +28,18 @@
     name))
 
 
-(defun %get-headers ()
-  (when *token*
-    (list (cons "Authorization"
-                (fmt "token ~A" *token*)))))
-
-
 (defun get-branches (url)
   "Returns two values: a list of branches and default branch.
 
    If repository not found, returns NIL."
   (handler-case
-      (let ((name (extract-github-name url))
-            (headers (%get-headers)))
+      (let ((name (extract-github-name url)))
         (when name
           (values
-           (loop for item in (get-json (fmt "https://api.github.com/repos/~A/branches" name)
-                                       :headers headers)
+           (loop for item in (github:get "/repos/~A/branches" :params (list name))
                  collect (getf item :|name|) into results
                  finally (return (sort results #'string<)))
-           (getf (get-json (fmt "https://api.github.com/repos/~A" name)
-                           :headers headers)
+           (getf (github:get "/repos/~A" :params (list name))
                  :|default_branch|))))
     (dexador:http-request-not-found ()
       nil)))
